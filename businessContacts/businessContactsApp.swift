026efeee9fd10ca3e2a8bf1cd95b1366c2796e97
Benia Morgan <bmorganware@gmail.com>
2023-08-25 13:56:38 -0400

    Initial Commit

---
+++ businessContacts/businessContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  businessContactsApp.swift
+//  businessContacts
+//
+//  Created by Benia Morgan-Ware on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct businessContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ businessContactsUITests/businessContactsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  businessContactsUITestsLaunchTests.swift
+//  businessContactsUITests
+//
+//  Created by Benia Morgan-Ware on 8/25/23.
+//
+
+import XCTest
+
+final class businessContactsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
