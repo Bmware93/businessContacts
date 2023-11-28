9f80d956429699d82878ea0b25eb8af9e025fe6f
Benia Morgan <bmorganware@gmail.com>
2023-11-27 22:21:17 -0500

    add contact page added

---
+++ businessContacts/ContactViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  ContactViewModel.swift
+//  businessContacts
+//
+//  Created by Benia Morgan-Ware on 8/26/23.
+//
+
+import Foundation
+import SwiftUI
+
+class ContactViewModel: ObservableObject, Identifiable {
+    @Published var contacts = [Contact]()
+    
+    var newContact: Contact?
+    
+    func saveContact() {
+        if let newContact = newContact {
+            contacts.append(newContact)
+        }
+    
+        
+    }
+}
+
+
